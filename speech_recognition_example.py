a63bf55237ffbc622dc99588d8a4228a06540520
benibenj <bsimmond@student.ethz.ch>
2020-02-06 18:24:05 +0100

    jarvis

--- speech_recognition_example.py
+++ speech_recognition_example.py
@@ -4,35 +4,54 @@ import time
 import playsound
 from gtts import gTTS
 
+r1 = sr.Recognizer()
+r2 = sr.Recognizer()
+
+running = True
+
 def speak(text):
     tts = gTTS(text=text, lang="en")
     filename = "voice.mp3"
     tts.save(filename)
     playsound.playsound(filename)
 
-r = sr.Recognizer()
 
-running = True
+def command():
+    with sr.Microphone() as source2:
+        print("listening")
 
+        audio2 = r2.listen(source2)
+
+        
+        text2 = r2.recognize_google(audio2)
+        text2 = text2.lower()
+        
+        if "stop" in text2:
+            running = False
+
+        print(text2)
+
+
+
+print("Starting...")
 while running:
     with sr.Microphone() as source:
-        print("Say something")
-        audio = r.listen(source)
+
+        audio = r1.listen(source)
 
         try:
-            text = r.recognize_google(audio)
-            print("You said : {}".format(text))
+            text = r1.recognize_google(audio)
+            text = text.lower()
 
             if "stop" in text:
                 running = False;
                 break;
 
-            speak(text)
-
+            if "jarvis" in text:
+                command()
 
         except sr.UnknownValueError:
-            print("sorry couldn't recognize that")
+            print("...")
         except sr.RequestError as e:
             print(e)
-            print("sorry couldn't recognize that")
     
