0a8f70498fd0467a6f6914ab812d7257230ce0db
benibenj <bsimmond@student.ethz.ch>
2020-02-06 18:01:01 +0100

    speech recognitaion and speaking

--- speech_recognition_example.py
+++ speech_recognition_example.py
@@ -1,4 +1,14 @@
 import speech_recognition as sr
+import os
+import time
+import playsound
+from gtts import gTTS
+
+def speak(text):
+    tts = gTTS(text=text, lang="en")
+    filename = "voice.mp3"
+    tts.save(filename)
+    playsound.playsound(filename)
 
 r = sr.Recognizer()
 
@@ -12,9 +22,14 @@ while running:
         try:
             text = r.recognize_google(audio)
             print("You said : {}".format(text))
+
             if "stop" in text:
                 running = False;
                 break;
+
+            speak(text)
+
+
         except sr.UnknownValueError:
             print("sorry couldn't recognize that")
         except sr.RequestError as e:
