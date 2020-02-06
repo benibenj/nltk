dceed1f7092d63fd13f97919442a854bf35c5c4a
benibenj <bsimmond@student.ethz.ch>
2020-02-06 15:22:36 +0100

    twitter analysis

---
+++ sentimentals_pos_neg/sentiment_twitter.py
@@ -0,0 +1,41 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+import sentiment_mod as s
+
+
+
+
+#consumer key, consumer secret, access token, access secret.
+ckey="KvVdfB4DtxvzxvsKhaAEJRlK4"
+csecret="y6ys8nryuk38NhRGuJOiOT6nNJ4ChOfYvXl5SvkT1gW3yOxKEO"
+atoken="944854539055886338-C0KshuXpjPmNAWprSN0Lxr1r0CLcR8c"
+asecret="YNMovhC6vbztJMDdZ2B4Gc4gtCGqRLvfXEBLXe2ix4LgG"
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        all_data = json.loads(data)
+
+        tweet = all_data["text"]
+
+        sentiment_value, confidence = s.sentiment(tweet)
+        print(tweet, sentiment_value, confidence)
+
+        if confidence >= 0.8:
+            output = open("twitter-out.txt", "a")
+            output.write(sentiment_value)
+            output.write("\n")
+            output.close()
+
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["1917"])
