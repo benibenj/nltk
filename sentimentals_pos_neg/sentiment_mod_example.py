2f5abd4521cea9a2bbfe38f16785e44a9b2b6f5b
benibenj <bsimmond@student.ethz.ch>
2020-02-17 11:10:14 +0100

    new print

--- sentimentals_pos_neg/sentiment_mod_example.py
+++ sentimentals_pos_neg/sentiment_mod_example.py
@@ -3,3 +3,5 @@ import sentiment_mod as s
 print(s.sentiment("This movie was awesome! The acting was great, plot was wonderful, and there story was good"))
 print(s.sentiment("This movie was terrible. There were absolutely 0 storyline. I don't see what the point was at all. Horrible movie, 0/10"))
 print(s.sentiment("The movie was great! I loved all of the actors! Ironman is always very cool to watch. But I didn't like the action."))
+
+print(s.sentiment("This movie is as awesome as the Emoji movie."))
